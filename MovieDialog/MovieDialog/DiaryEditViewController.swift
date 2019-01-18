221533df13479afd71432cd8868699c6a8e5c353
PMinJoo <pmj731nm@gmail.com>
2019-01-18 16:40:14 +0900

    일기 추가-사진 선택 기능 구현
    영화 제목 검색 후 알맞은 영화 선택시 Diary Edit 화면에 해당 영화의 포스터와 제목 출력되게 함

--- MovieDialog/MovieDialog/DiaryEditViewController.swift
+++ MovieDialog/MovieDialog/DiaryEditViewController.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class DiaryEditViewController: UIViewController {
+class DiaryEditViewController: UIViewController, SendDataDelegate {
     @IBOutlet weak var movieImage: UIImageView!
     @IBOutlet weak var movieTitle: UILabel!
-    @IBAction func selectPicButton(_ sender: UIButton) {
+    @IBAction func selectPicButton(_ sender: UIButton) { //Alert Controller
         let actionSheet = UIAlertController(title:"포스터 입력 방식을 선택해 주세요", message:nil, preferredStyle:.actionSheet)
         actionSheet.addAction(UIAlertAction(title:"웹에서 검색하기", style:.default, handler:{result in
-            //do something
-            
+            //검색창 띄우기
+            if let _ = self.storyboard!.instantiateViewController(withIdentifier: "SearchViewID") as? SearchViewController{
+                //self.present(searchController, animated:true, completion:nil)
+                self.performSegue(withIdentifier: "toSearchSegue", sender: nil)
+            }
         }))
         actionSheet.addAction(UIAlertAction(title:"내 파일에서 불러오기", style:.default, handler:{result in
             //do something22
@@ -29,6 +32,17 @@ class DiaryEditViewController: UIViewController {
 
     }
     
-
+    func sendData(title:String, img:UIImage){
+        movieTitle.text = title
+        movieImage.image = img
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSearchSegue"{
+            let nav:UINavigationController = segue.destination as! UINavigationController
+            let viewController = nav.viewControllers[0] as! SearchViewController
+            viewController.delegate = self
+        }
+    }
 
 }
--- MovieDialog/MovieDialog/SearchViewController.swift
+++ MovieDialog/MovieDialog/SearchViewController.swift
@@ -9,13 +9,26 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    var delegate:SendDataDelegate?
+    
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBAction func searchButtonPressed(_ sender: Any) {
+        if let _ = searchTextField.text{
+            performSegue(withIdentifier:"searchSegue", sender:self)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        //Dispose of any resources that can be recreated.
+    }
 
     /*
     // MARK: - Navigation
@@ -26,5 +39,15 @@ class SearchViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    //MoviesTableViewController의 queryText 필드에 텍스트 필드의 내용을 저장해줌으로써 다음 뷰로 검색어를 넘김
+    override func prepare(for segue:UIStoryboardSegue, sender:Any?){
+        if let moviesVC = segue.destination as? MoviesTableViewController{
+            if let text = searchTextField.text{
+                moviesVC.delegate = delegate
+                moviesVC.queryText = text
+            }
+        }
+    }
 
 }
