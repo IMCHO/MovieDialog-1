1f871b6b80913bdbaca7f7d257786ef2bd71ef91
In Taek Cho <dlsxor21c@naver.com>
2019-01-20 15:48:45 +0900

    Info 탭에 정보 출력 완료

--- MovieDialog/MovieDialog/InfoTableViewController.swift
+++ MovieDialog/MovieDialog/InfoTableViewController.swift
@@ -34,15 +34,16 @@ class InfoTableViewController: UITableViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
+        
         if let data=try? Data(contentsOf: URL(fileURLWithPath:documentsPath+"/dialog.plist")){
             if let decodedDialogs=try? decoder.decode([Dialog].self, from: data){
                 dialogs=decodedDialogs
                 
-                print(dialogs.count)
+//                print(dialogs.count)
 
-                print(dialogs.filter({$0.review != ""}).count)
+//                print(dialogs.filter({$0.review != ""}).count)
                 
-                print(dialogs.filter({$0.simpleReview.count>0}).count)
+//                print(dialogs.filter({$0.simpleReview.count>0}).count)
                 
                 for dialog in dialogs{
                     if let times=timesOfMovie[dialog.title]{
@@ -52,7 +53,7 @@ class InfoTableViewController: UITableViewController {
                     }
                 }
                 
-                print(timesOfMovie.sorted{$0.1>$1.1})
+//                print(timesOfMovie.sorted{$0.1>$1.1})
             }
         }
         
@@ -60,14 +61,38 @@ class InfoTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 4
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell=tableView.dequeueReusableCell(withIdentifier: "defaultCell", for: indexPath)
+        cell.detailTextLabel?.textColor = .blue
+        
+        if indexPath.row == 0 {
+            cell.textLabel?.text="관람 작품 수"
+            cell.detailTextLabel?.text = String(dialogs.count)+" 개"
+        }else if indexPath.row==1{
+            cell.textLabel?.text="작성 리뷰 수"
+            cell.detailTextLabel?.text=String(dialogs.filter({$0.review != ""}).count)+" 개"
+        }else if indexPath.row==2{
+            cell.textLabel?.text="작성 간편리뷰 수"
+            cell.detailTextLabel?.text = String(dialogs.filter({$0.simpleReview.count>0}).count)+" 개"
+        }else{
+            cell.textLabel?.text="최다 관람 순위"
+            cell.detailTextLabel?.text = timesOfMovie.sorted{$0.1>$1.1}[0].0
+        }
+        
+        
+   
+        
+        return cell
+    }
  
 
 }
