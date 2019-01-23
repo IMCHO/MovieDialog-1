4f48306282257ae4228d1257afc1805f46b89a41
지호 <jiho8345@naver.com>
2019-01-23 21:29:01 +0900

    목표기간설정

--- MovieDialog/MovieDialog/AddGoalView.swift
+++ MovieDialog/MovieDialog/AddGoalView.swift
@@ -27,6 +27,48 @@ class AddGoalView: UIViewController {
         inputNum.text = ""
     }
     
+    //목표 기간 선택
+    @IBOutlet weak var startDay: UILabel!
+    @IBOutlet weak var finishDay: UILabel!
+    
+    //시작 날짜 선택
+    @IBAction func startDate(_ sender: Any) {
+        let startDate: UIDatePicker = UIDatePicker()
+        startDate.datePickerMode = .date
+        
+        let alert = UIAlertController(title: "\n\n\n\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+        alert.view.addSubview(startDate)
+        
+        alert.addAction(UIAlertAction(title:"완료", style:.default, handler:{result in
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy.MM.dd"
+            let dateString = dateFormatter.string(from:startDate.date)
+            self.startDay.text = dateString //시작 날짜 받아와서 저장
+            
+        }))
+        alert.addAction(UIAlertAction(title:"취소", style:.cancel, handler:nil))
+        self.present(alert, animated:true, completion:nil)
+    }
+    
+    //종료 날짜 선택
+    @IBAction func finishDate(_ sender: Any) {
+        let finishDate: UIDatePicker = UIDatePicker()
+        finishDate.datePickerMode = .date
+        
+        let alert = UIAlertController(title: "\n\n\n\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+        alert.view.addSubview(finishDate)
+        
+        alert.addAction(UIAlertAction(title:"완료", style:.default, handler:{result in
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy.MM.dd"
+            let dateString = dateFormatter.string(from:finishDate.date)
+            self.finishDay.text = dateString //종료 날짜 받아와서 저장
+            
+        }))
+        alert.addAction(UIAlertAction(title:"취소", style:.cancel, handler:nil))
+        self.present(alert, animated:true, completion:nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
