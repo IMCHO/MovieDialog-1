212ec1858116487fe27f990dca94310668b75427
PMinJoo <pmj731nm@gmail.com>
2019-01-23 14:15:59 +0900

    일기 확인 화면 UI 구성 중

---
+++ MovieDialog/MovieDialog/ShowDiaryViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ShowDiaryViewController.swift
+//  MovieDialog
+//
+//  Created by linc on 23/01/2019.
+//  Copyright © 2019 linc. All rights reserved.
+//
+
+import UIKit
+
+class ShowDiaryViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageLabel: UIImageView!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
