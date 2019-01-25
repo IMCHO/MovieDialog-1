bd5c1641685c687445a156750d427e9d71d93ad0
PMinJoo <pmj731nm@gmail.com>
2019-01-25 18:04:07 +0900

    영화 제목 입력시 키보드 올라오며 화면 이동하는 것 제거

--- MovieDialog/MovieDialog/DiaryEditViewController.swift
+++ MovieDialog/MovieDialog/DiaryEditViewController.swift
@@ -409,8 +409,6 @@ class DiaryEditViewController: UIViewController, SendDataDelegate, UITextFieldDe
         picker.delegate = self as UIImagePickerControllerDelegate & UINavigationControllerDelegate
         reviewInputText.delegate = self
         
-        
-        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name:UIResponder.keyboardWillShowNotification, object:nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name:UIResponder.keyboardWillHideNotification, object:nil)
     }
@@ -446,7 +444,9 @@ class DiaryEditViewController: UIViewController, SendDataDelegate, UITextFieldDe
     }
 
     @objc func keyboardWillShow(_ sender:Notification){
-        self.view.frame.origin.y = -150 //Move view 150 points upward
+        if reviewInputText.isFirstResponder{
+            self.view.frame.origin.y = -220 //Move view 150 points upward
+        }
     }
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         reviewInputText.resignFirstResponder()
